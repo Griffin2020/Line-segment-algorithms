3543439c774d1120063f0697f2a1fe834f9c3beb
Griffin2020 <griffin.sober@gmail.com>
2023-03-16 12:06:47 -0400

    testing

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -33,15 +33,16 @@ int orientation(int x1, int x2, int x3, int y1, int y2, int y3){
     return 1;
 }
 tuple <double, double> findIntersections(lineStruct lineOne, lineStruct lineTwo, double safe_number){
+ 
 
     double oneSlope = (lineOne.y1 - lineOne.y2) / (lineOne.x1 - lineOne.x2);
     double twoSlope = (lineTwo.y1 - lineTwo.y2) / (lineTwo.x1 - lineTwo.x2);
     if(oneSlope == twoSlope){
         return {safe_number, safe_number};
     }
- 
-    double x = ((lineOne.x1 * oneSlope) - (lineTwo.x1 * twoSlope) + (lineTwo.y2 - lineOne.y1)) / (oneSlope - twoSlope);
+    double x = ((lineOne.x1 * oneSlope) - (lineTwo.x1 * twoSlope) + (lineTwo.y1 - lineOne.y1)) / (oneSlope - twoSlope);
     if(!(lineOne.x1 < x && lineOne.x2 > x)){
+
         return {safe_number, safe_number};
     }
     if(!(lineTwo.x1 < x && lineTwo.x2 > x)){
@@ -58,17 +59,18 @@ int countIntersections(lineStruct linesList[], int numLines, double safe_number)
     for(int i = 0; i < numLines - 1; i++){
         for(int j = i + 1; j < numLines; j++){
             tie(a, b) = findIntersections(linesList[i], linesList[j], safe_number);
-            cout << a << " " << b << endl;
-            if(a != safe_number && b != safe_number){
+            //cout << a << " " << b << endl;
+            if(a != safe_number){
                 intersections.push_back({a, b});
                 numIntersections++;
             }
             
         }
     }
+    cout << "Intersections found: " << endl;
     for(auto x : intersections){
         tuple <double,double> tp = x;
-        cout << get<0>(tp) << endl;
+        cout << get<0>(tp) << ", " << get<1>(tp) << endl;
     }
     
     return numIntersections;
@@ -76,16 +78,31 @@ int countIntersections(lineStruct linesList[], int numLines, double safe_number)
 }
 int main() {
     int dimensions = 10; // Dimensions of the square
-    int numLines = 7; // Number of lines
+    int numLines = 2; // Number of lines
     lineStruct linesList[numLines];
     srand(1);
-    for(int i = 0; i < numLines; i++){
+    list<lineStruct> usedLines;
+    /* for(int i = 0; i < numLines; i++){
+
         linesList[i].x1 = rand() % dimensions;
         linesList[i].y1 = rand() % dimensions;
         linesList[i].x2 = rand() % dimensions;
         linesList[i].y2 = rand() % dimensions;
-        cout << linesList[i].x1 << " " << linesList[i].y1 << " " << linesList[i].x2 << " " << linesList[i].y2 << endl;
-    }
-    cout << "There are a total of " << countIntersections(linesList, numLines, dimensions + 1) << " intersections" << endl;   
+        while(linesList[i].x1 == linesList[i].x2){
+            linesList[i].x2 = rand() % dimensions;
+        }
+    } */
+    linesList[0].x1 = 2;
+    linesList[0].x2 = 5;
+    linesList[0].y1 = 2;
+    linesList[0].y2 = 6;
+    
+    linesList[1].x1 = 1;
+    linesList[1].x2 = 5;
+    linesList[1].y1 = 8;
+    linesList[1].y2 = 1;
+
+
+    countIntersections(linesList, numLines, dimensions + 1);
     return 0;
 }
